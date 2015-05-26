ceac0698970985cad8ee768620ebed02ef622ae7
Wil Wade <wil@supplyhog.com>
2015-05-26 07:57:25 -0400

    Example of Komodo failing autocomplete with Codeception

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Codeception\TestCase\Test;
+
+class ExampleTest extends Test
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+        $this->assertTrue(false);
+    }
+
+}
